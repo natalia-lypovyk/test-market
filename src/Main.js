ec913b4c7010e2fe3046c52cd5e48690017bb835
Natalia Lypovyk <cardinnata@gmail.com>
2021-11-03 11:24:08 +0200

    feat: Connect to metamask with connector

--- src/Main.js
+++ src/Main.js
@@ -1,14 +1,20 @@
 import React from 'react';
+import { Web3ReactProvider } from '@web3-react/core';
+import { Web3Provider } from '@ethersproject/providers';
 
 import RenderRoutes from './components/RenderRoutes';
 import Layout from './layout/Layout';
 import routes from './routes';
 import './styles/Main.scss';
 
+const getLibrary = (provider) => new Web3Provider(provider);
+
 const Main = () => (
-  <Layout>
-    <RenderRoutes routes={routes} />
-  </Layout>
+  <Web3ReactProvider getLibrary={getLibrary}>
+    <Layout>
+      <RenderRoutes routes={routes} />
+    </Layout>
+  </Web3ReactProvider>
 );
 
 export default Main;
--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,5 +1,38 @@
 import React from 'react';
+import { useWeb3React } from '@web3-react/core';
 
-const Home = () => <h1>Home</h1>;
+import injected from '../../connector';
+
+const Home = () => {
+  const { activate, deactivate, active, library } = useWeb3React();
+
+  return (
+    <>
+      <h1>Home</h1>
+
+      {active ? (
+        <>
+          <p>Connected with {library.connection.url}</p>
+
+          <button
+            className="connect-button"
+            onClick={() => deactivate()}
+            type="button"
+          >
+            Disconnect
+          </button>
+        </>
+      ) : (
+        <button
+          className="connect-button"
+          onClick={() => activate(injected)}
+          type="button"
+        >
+          Connect to wallet
+        </button>
+      )}
+    </>
+  );
+};
 
 export default Home;
